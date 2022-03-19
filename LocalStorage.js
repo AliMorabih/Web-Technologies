a0c56765e4447df932bacb3af9df01bbd5bef9f6
“Ali <40522091@live.napier.ac.uk>
2022-03-19 14:32:03 -0400

    update the Js code for the login

--- LocalStorage.js
+++ LocalStorage.js
@@ -6,6 +6,7 @@ function DataStorage(){
 	name=document.getElementById("name").value;
 	email=document.getElementById("email").value;
 	psw=document.getElementById("psw").value;
+	
 
 	let user_records=new Array();
 	user_records=JSON.parse(localStorage.getItem("key"))?JSON.parse(localStorage.getItem("key")):[]
@@ -13,6 +14,16 @@ function DataStorage(){
 	if(user_records.some((v)=>{return v.email==email})){
 	  alert("This Email address already exist in our Storage");
 	}
+		if (name==null || name=="" || psw==null || psw=="" ){  
+		   alert(" Name or Password cannot be blank Please try again. ");  
+		   return false;
+		}else if (email==null || email==""  ){  
+		   alert(" Email address cannot be empty");  
+		   return false;   
+		}else if(psw.length<6 || psw.length>18){  
+		   alert(" Password should be between 6 and 18 characters Try again.");  
+		   return false;  
+		}
 	else{
 	  user_records.push({
 	  "name":name,
@@ -21,6 +32,5 @@ function DataStorage(){
 	})
 	localStorage.setItem("key",JSON.stringify(user_records));
 	}
-
 	}
-        
+	
