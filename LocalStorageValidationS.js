87dfc1d148eb69313470057d9c825ab566abfa93
“Ali <40522091@live.napier.ac.uk>
2022-03-19 11:44:10 -0400

    set up new Branch

---
+++ LocalStorageValidationS.js
@@ -0,0 +1,27 @@
+
+
+function SignIn()
+{
+	
+	let email,psw;
+
+	email=document.getElementById("email").value;
+	psw=document.getElementById("psw").value;
+
+	let user_records=new Array();
+	user_records=JSON.parse(localStorage.getItem("key"))?JSON.parse(localStorage.getItem("key")):[]
+
+	if(user_records.some((v)=>{return v.email==email && v.psw==psw}))
+	{
+	  alert("Login Pass");
+	  let current_user=user_records.filter((v)=>{return v.email==email && v.psw==psw})[0]
+	 localStorage.setItem('name',current_user.name);
+	 localStorage.setItem('email',current_user.email);
+	  window.location.href='file:///C:QuizProject/QuizPage.html'
+	}
+	else
+	{
+	  alert('Login Fail');
+	}
+
+}
