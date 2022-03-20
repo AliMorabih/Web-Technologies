4b3952f9d2b9f8c0cff0fc2c60c768bd75850427
“Ali <40522091@live.napier.ac.uk>
2022-03-20 15:28:25 -0400

    Add files

--- LocalStorageValidationS.js
+++ LocalStorageValidationS.js
@@ -17,11 +17,7 @@ function SignIn()
 	  let current_user=user_records.filter((v)=>{return v.email==email && v.psw==psw})[0]
 	 localStorage.setItem('name',current_user.name);
 	 localStorage.setItem('email',current_user.email);
-<<<<<<< HEAD
 	  window.location.href='file:///C:QuizProject/QuizPage.html'
-=======
-	  window.location.href="file:///C:/Users/Ali/OneDrive%20-%20Edinburgh%20Napier%20University/Desktop/PHP/QuizProject/QuizPage.html"
->>>>>>> 9ad84419dea57be858c731221e9077d41b0162e6
 	}
 	else
 	{
--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 # Web-Technologies
-=======
-# Web-Technologies
->>>>>>> 9ad84419dea57be858c731221e9077d41b0162e6
